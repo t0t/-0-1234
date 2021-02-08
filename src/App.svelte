6ff4a302f7daecae76a05285be6fc6f58ef14eac
t0t <sergiofores@gmail.com>
2021-02-08 11:39:07 +0100

    base window parameters

--- src/App.svelte
+++ src/App.svelte
@@ -1,36 +1,52 @@
 <script>
-	import P5 from 'p5-svelte';
-	let width = 55;
-	let height = 55;
-  
-	const sketch = (p5) => {
-	  p5.setup = () => {
-		p5.createCanvas(400, 400);
-	  };
-  
-	  p5.draw = () => {
-		p5.background(220)
-		p5.ellipse(p5.width / 2, p5.height / 2, width, height);
-		// console.log(p5.width)
-	  };
-	};
-  </script>
-  <style>
-	  canvas {
-		  width: 100%;
-	  }
-  </style>
-  <h1>Diseño generativo</h1>
-  <label>
-	Width
-	<input type="range" bind:value={width} min="100" max="1000" step="0.01" />
-	{width}
-  </label>
-  
-  <label>
-	Height
-	<input type="range" bind:value={height} min="100" max="1000" step="0.01" />
-	{height}
-  </label>
-  
+  import P5 from 'p5-svelte'
+  let width = 0
+  let height = 0
+
+  const sketch = (p5) => {
+    p5.setup = () => {
+      p5.createCanvas(width, height)
+    }
+
+    p5.draw = () => {
+      p5.ellipse(p5.width, p5.height, width, height)
+      p5.line(0, 0, 400, 400)
+      p5.line(0, 400, 400, 0)
+    }
+  }
+</script>
+
+<style>
+  .canvas {
+    width: 100vw;
+    height: 100vh;
+  }
+  body {
+    overflow: hidden;
+  }
+  .over {
+    position: fixed;
+  }
+</style>
+
+
+<div class="canvas" bind:clientWidth={width} bind:clientHeight={height}>
+  <div class="over">
+    <h1>Diseño generativo</h1>
+
+      <input type="range" bind:value={width} min="100" max="1000" step="0.01" />
+      {width}
+
+      <input
+        type="range"
+        bind:value={height}
+        min="100"
+        max="1000"
+        step="0.01" />
+      {height}
+
+	  
+  </div>
+
   <P5 {sketch} />
+</div>
